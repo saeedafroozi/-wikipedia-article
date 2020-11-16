9f140f0e318fc1154dbb31a77a0f96588faff7ee
saeed afroozi <saeed.afroozi@gmail.com>
2020-11-16 13:37:27 +0330

    structuring the main component

--- client/src/view/App.tsx
+++ client/src/view/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import ResetCSS from '../asset/styles/Reset';
+import Layout from '../components/Layout';
+import styled from 'styled-components';
+import { StringDictionary } from '../utils/constants'
 
+const Header = styled.header`
+  text-align: center;
+  margin-top:20px;
+  margin-bottom:20px;
+`;
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+const Title = styled.h1`
+  font-size: 3rem;
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+`;
+
+const App: React.FC = () => {
+  return <>
+    <ResetCSS />
+    <Layout>
+      <Header >
+        <Title>
+          {StringDictionary.App_Title}
+        </Title>
+      </Header>
+    </Layout>
+  </>
+};
 
 export default App;

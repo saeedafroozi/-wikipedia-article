32d633837e166c3c52036368168e74d4a9f7fc0d
saeed afroozi <saeed.afroozi@gmail.com>
2020-11-16 12:52:09 +0330

    setuping apollo client to conect to server

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './view/App';
+import App from '../src/view/App';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import reportWebVitals from './reportWebVitals';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
@@ -15,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+

e1f3081bfe629e580c51d343cf9bc022ee2cddc0
saeed afroozi <saeed.afroozi@gmail.com>
2020-11-16 14:46:24 +0330

    implementing the main component to fetch data from apollo server with its dictionaries and its svgs icons

---
+++ client/src/components/SearchResult.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { ApolloError, gql, useQuery } from '@apollo/client';
+import styled from 'styled-components';
+import { StringDictionary } from '../utils/constants';
+import { ReactComponent as load } from '../asset/icons/three-dots.svg';
+
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 15px;
+`
+const Loading = styled(load)`
+width:100px;
+height:100px;
+`
+const NoResult = styled.div`
+`
+const Exception = styled.div`
+`
+export const ARTICLE_QUERY = gql`
+  query getArticle($page: String) {
+    article(page: $page) {
+      title
+      categories
+      sections {
+        title
+        id
+        indentLevel
+      }
+    }
+  }
+`;
+
+const renderRightComponent = (loading: boolean, error: ApolloError | undefined, data: GraphqlArticleViewModel | undefined) => {
+  if (loading) {
+    return <Loading>{StringDictionary.Waiting}</Loading>
+  }
+  if (error) {
+    return <Exception>{StringDictionary.Error_Execption}</Exception>
+  }
+  else {
+    const { article } = data!;
+    if (!article) {
+      return <NoResult>{StringDictionary.Not_found}</NoResult>
+    }
+    return (
+      <>
+       
+      </>
+    );
+  }
+}
+
+interface Props {
+  page: string;
+}
+
+const SearchResult: React.FC<Props> = ({ page }) => {
+  const { loading, error, data } = useQuery<GraphqlArticleViewModel>(
+    ARTICLE_QUERY,
+    {
+      variables: {
+        page
+      }
+    }
+  );
+  return (
+    <Container>{renderRightComponent(loading, error, data)}</Container>
+  )
+}
+
+export default SearchResult;
--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,3 +1,6 @@
 export const StringDictionary = {
   App_Title: "Browse Wikipedia Articles",
+  Not_found: "No Article Avialable...",
+  Waiting: "Please Wait...",
+  Error_Execption: "Server Is Busy Please Try Later..."
 }
